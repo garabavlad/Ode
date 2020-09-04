50218532133a658c6a020868f3b53c816409adcc
garaba.vlad <garaba.vlad@gmail.com>
2020-09-04 22:50:29 +0300

    First

---
+++ Pages/Restaurants/List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ode.Pages.Restaurants
+{
+    public class RestaurantsListModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
